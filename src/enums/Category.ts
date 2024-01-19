850030722fef64fd8897b6116649c73e3a5a2f1a
HighError <hierrorua@gmail.com>
2024-01-19 12:52:28 +0200

    perf: ⚡️ Docker build tested

--- src/enums/Category.ts
+++ src/enums/Category.ts
@@ -1,5 +1,9 @@
 enum Category {
   Utilities = 'Utilities',
+  LethalCompany = 'Lethal Company',
+  Valorant = 'Valorant',
+  Moderators = 'Moderators',
+  Fun = 'Fun',
 }
 
 export default Category;
